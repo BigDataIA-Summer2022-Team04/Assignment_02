781a711a44a80529239de2af0c27bdf26ea82974
Piyush Anand <anand.pi@northeastern.edu>
2022-06-24 21:23:25 -0400

    unit testing v2

--- API/custom_functions.py
+++ API/custom_functions.py
@@ -1,11 +1,15 @@
+from google.cloud import bigquery
+from dotenv import load_dotenv
+import os
+
 #################################################
-# Author: Piyush
+# Author: Abhijit, Piyush
 # Creation Date: 17-Jun-22
 # Last Modified Date:
 # Change Logs:
 # SL No         Date            Changes
 # 1             17-Jun-22       First Version
-# 
+# 2             24-Jun-22       Log Function
 #################################################
 
 def validate_state(name: str):
@@ -27,3 +31,21 @@ def validate_state(name: str):
     return name.lower() in (item.lower() for item in all_states)
 
 
+def logfunc(endpoint:str, response_code: int):
+    load_dotenv()
+    os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = os.getenv('BQ_KEY_JSON')
+    client = bigquery.Client()
+    max=client.query(f"select max(logid)+1, string(current_timestamp()) as tstamp from `plane-detection-352701.SPY_PLANE.logs`").result()
+    for i in max:
+        var= i[0]
+        tstamp=i[1]
+    print(var)
+    rows_to_insert =[{"logtime":tstamp, "endpoint": endpoint, "response_code": response_code,"logid":var}]
+    errors = client.insert_rows_json('plane-detection-352701.SPY_PLANE.logs', rows_to_insert)  # Make an API request.
+    if errors == []:
+        print("New rows have been added.")
+    else:
+        print("Encountered errors while inserting rows: {}".format(errors))
+
+
+# logfunc('hello', 101)
--- API/routers/data.py
+++ API/routers/data.py
@@ -2,19 +2,22 @@
 import logging
 from typing import Union
 from google.cloud import bigquery
-from validate_state import validate_state
+from custom_functions import validate_state, logfunc
 from fastapi import APIRouter, HTTPException, Response, status, Query
+
+
 
 
 router = APIRouter(
     prefix="/data",
     tags=['Data']
 )
-
+# /Applications/Python 3.9/Python Launcher.app
 
 @router.get('/registrant', status_code=status.HTTP_200_OK)
 async def get_registrant(user_list: Union[list[str], None] = Query(default=None), if_records: bool = False):
     if not user_list:
+        logfunc("/data/registrant", 400)
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,detail="User Input is NULL" )
     filtered_state = []
     for states in user_list:
@@ -22,6 +25,7 @@ async def get_registrant(user_list: Union[list[str], None] = Query(default=None)
             filtered_state.append(states.upper())
     state_code = "', '".join(filtered_state)
     if not filtered_state:
+        logfunc("/data/registrant", 400)
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,detail="User Input is Invalid, Verify the Inputs")
     else:
         logging.info(f"User passed state, {filtered_state} is valid")
@@ -31,6 +35,7 @@ async def get_registrant(user_list: Union[list[str], None] = Query(default=None)
         except Exception as e:
             logging.error(f"Check the path of the JSON file and contents")
             logging.error(f"Cannot connect to Big Query Server")
+            logfunc("/data/registrant", 500)
             raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
             # return 101
         formated_query = f"""WITH faa AS (
@@ -68,22 +73,27 @@ async def get_registrant(user_list: Union[list[str], None] = Query(default=None)
             df = client.query(formated_query).to_dataframe()
         except Exception as e:
             logging.error(f"Bad SQL Query, Please verify SQL")
+            logfunc("/data/registrant", 500)
             raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
         if df.empty:
             logging.error(f"No rows returned from big query")
+            logfunc("/data/registrant", 204)
             raise HTTPException(status_code=status.HTTP_204_NO_CONTENT)
             # return 103
         if if_records:
+            logfunc("/data/registrant", 200)
             return Response(df.to_json(orient="records"), media_type="application/json")
         logging.info(f"Aggregating data from dataframe")
         df2 = df.groupby(['TYPE_REGISTRANT'])['TYPE_REGISTRANT'].count().reset_index(name='count').sort_values(['count'], ascending=False) 
         logging.info(f"Returning dataframe as JSON")
+        logfunc("/data/registrant", 200)
         return Response(df2.to_json(orient="records"), media_type="application/json")
 
 
 @router.get('/aircraft', status_code=status.HTTP_200_OK)
 async def get_aircraft(user_list: Union[list[str], None] = Query(default=None), if_records: bool = False):
     if not user_list:
+        logfunc("/data/aircraft", 400)
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,detail="User Input is NULL" )
     filtered_state = []
     for states in user_list:
@@ -91,6 +101,7 @@ async def get_aircraft(user_list: Union[list[str], None] = Query(default=None), 
             filtered_state.append(states.upper())
     state_code = "', '".join(filtered_state)
     if not filtered_state:
+        logfunc("/data/aircraft", 400)
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,detail="User Input is Invalid, Verify the Inputs")
     else:
         logging.info(f"User passed state, {state_code} is valid")
@@ -100,6 +111,7 @@ async def get_aircraft(user_list: Union[list[str], None] = Query(default=None), 
         except Exception as e:
             logging.error(f"Check the path of the JSON file and contents")
             logging.error(f"Cannot connect to Big Query Server")
+            logfunc("/data/aircraft", 500)
             raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
             # return 101
         formated_query = f"""WITH faa AS (
@@ -138,23 +150,28 @@ async def get_aircraft(user_list: Union[list[str], None] = Query(default=None), 
             df = client.query(formated_query).to_dataframe()
         except Exception as e:
             logging.error(f"Bad SQL Query, Please verify SQL")
+            logfunc("/data/aircraft", 500)
             raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
             # return 104
         if df.empty:
             logging.error(f"No rows returned from big query")
+            logfunc("/data/aircraft", 204)
             raise HTTPException(status_code=status.HTTP_204_NO_CONTENT)
             # return 103
         if if_records:
+            logfunc("/data/aircraft", 200)
             return Response(df.to_json(orient="records"), media_type="application/json")
         logging.info(f"Aggregating data from dataframe")
         df2 = df.groupby(['TYPE_AIRCRAFT'])['TYPE_AIRCRAFT'].count().reset_index(name='count').sort_values(['count'], ascending=False) 
         logging.info(f"Returning dataframe as JSON")
+        logfunc("/data/aircraft", 200)
         return Response(df2.to_json(orient="records"), media_type="application/json")
 
 
 @router.get('/engine', status_code=status.HTTP_200_OK)
 async def get_engine(user_list: Union[list[str], None] = Query(default=None), if_records: bool = False):
     if not user_list:
+        logfunc("/data/aircraft", 400)
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,detail="User Input is NULL" )
     filtered_state = []
     for states in user_list:
@@ -162,6 +179,7 @@ async def get_engine(user_list: Union[list[str], None] = Query(default=None), if
             filtered_state.append(states.upper())
     state_code = "', '".join(filtered_state)
     if not filtered_state:
+        logfunc("/data/aircraft", 400)
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,detail="User Input is Invalid, Verify the Inputs")
     else:
         logging.info(f"User passed state, {state_code} is valid")
@@ -171,6 +189,7 @@ async def get_engine(user_list: Union[list[str], None] = Query(default=None), if
         except Exception as e:
             logging.error(f"Check the path of the JSON file and contents")
             logging.error(f"Cannot connect to Big Query Server")
+            logfunc("/data/aircraft", 500)
             raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
             # return 101
         formated_query = f"""WITH faa AS (
@@ -208,16 +227,20 @@ async def get_engine(user_list: Union[list[str], None] = Query(default=None), if
             df = client.query(formated_query).to_dataframe()
         except Exception as e:
             logging.error(f"Bad SQL Query, Please verify SQL")
+            logfunc("/data/aircraft", 500)
             raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
             # return 104
         if df.empty:
             logging.error(f"No rows returned from big query")
+            logfunc("/data/aircraft", 204)
             raise HTTPException(status_code=status.HTTP_204_NO_CONTENT)
             # return 103
         if if_records:
+            logfunc("/data/aircraft", 200)
             return Response(df.to_json(orient="records"), media_type="application/json")
         logging.info(f"Aggregating data from dataframe")
         df2 = df.groupby(['TYPE_ENGINE'])['TYPE_ENGINE'].count().reset_index(name='count').sort_values(['count'], ascending=False) 
         logging.info(f"Returning dataframe as JSON")
+        logfunc("/data/aircraft", 200)
         return Response(df2.to_json(orient="records"), media_type="application/json")
 
