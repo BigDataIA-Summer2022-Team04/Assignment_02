3d6cf9fc0dd23c3a7ce7537c923a74a0d9472e93
Piyush Anand <anand.pi@northeastern.edu>
2022-07-16 00:58:43 -0400

    bug fix

--- main.py
+++ main.py
@@ -136,5 +136,5 @@ app.layout = html.Div(
 
 # app run
 # if __name__ == '__main__':
-app.run_server(debug=True)
+app.run_server(debug=True, host='0.0.0.0')
 # app.run_server(debug=True, host='0.0.0.0', port = 8051)
